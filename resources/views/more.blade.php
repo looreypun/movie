5db23b6514acbe7ad442d7adb3cc2f9fd4d7747c
Himal Pun <45704147+looreypun@users.noreply.github.com>
2023-08-08 18:21:38 +0900

    Update more.blade.php

--- resources/views/more.blade.php
+++ resources/views/more.blade.php
@@ -5,14 +5,14 @@
 @if(isset($popular_data))
     <div class="movie_category pt-3 px-4">
         <p class="btn btn-danger">{{$name}}</p>
-        <a href="/index"><p class="btn btn-dark float-right ">戻る</p></a>
+        <a href="index"><p class="btn btn-dark float-right ">戻る</p></a>
     </div>
     <div class="d-flex justify-content-center">{{$popular_data->links()}}</div>
     <div id="popular" class="row px-4">
         @foreach($popular_data as $item)
         @if(!empty($item->poster_path))
             <div  class="col-6 col-md-3 col-xl-2">
-            <a href="/play/{{$item->id}}"><img class="display_img" src="https://image.tmdb.org/t/p/w500{{$item->poster_path}}"  width="100%" height="100%" ></a>
+            <a href="play/{{$item->id}}"><img class="display_img" src="https://image.tmdb.org/t/p/w500{{$item->poster_path}}"  width="100%" height="100%" ></a>
             </div>
         @endif
         @endforeach
@@ -22,14 +22,14 @@
 @elseif(isset($trending_data))
     <div class="movie_category pt-3 px-4">
         <p class="btn btn-danger">{{ $name }}</p>
-        <a href="/index"><p class="btn btn-dark float-right ">戻る</p></a>
+        <a href="index"><p class="btn btn-dark float-right ">戻る</p></a>
     </div>
     <div class="d-flex justify-content-center">{{$trending_data->links()}}</div>
     <div id="popular" class="row px-4 ">
         @foreach($trending_data as $item)
         @if(!empty($item->poster_path))
             <div  class="col-6 col-md-3 col-xl-2">
-            <a href="/play/{{$item->id}}"><img class="display_img" src="https://image.tmdb.org/t/p/w500{{$item->poster_path}}"  width="100%" height="100%" ></a>
+            <a href="play/{{$item->id}}"><img class="display_img" src="https://image.tmdb.org/t/p/w500{{$item->poster_path}}"  width="100%" height="100%" ></a>
             </div>
         @endif
         @endforeach
@@ -39,14 +39,14 @@
 @elseif(isset($now_playing_data))
     <div class=" movie_category pt-3 px-4">
         <p class="btn btn-danger">{{ $name }}</p>
-        <a href="/index"><p class="btn btn-dark float-right ">戻る</p></a>
+        <a href="index"><p class="btn btn-dark float-right ">戻る</p></a>
     </div>
     <div class="d-flex justify-content-center">{{$now_playing_data->links()}}</div>
     <div id="popular" class="row px-4">
         @foreach($now_playing_data as $item)
         @if(!empty($item->poster_path))
             <div  class="col-6 col-md-3 col-xl-2">
-            <a href="/play/{{$item->id}}"><img class="display_img" src="https://image.tmdb.org/t/p/w500{{$item->poster_path}}"  width="100%" height="100%" ></a>
+            <a href="play/{{$item->id}}"><img class="display_img" src="https://image.tmdb.org/t/p/w500{{$item->poster_path}}"  width="100%" height="100%" ></a>
             </div>
         @endif
         @endforeach
@@ -56,14 +56,14 @@
 @elseif(isset($upcoming_data))
     <div class=" movie_category pt-3 px-4">
         <p class="btn btn-danger">{{ $name }}</p>
-        <a href="/index"><p class="btn btn-dark float-right ">戻る</p></a>
+        <a href="index"><p class="btn btn-dark float-right ">戻る</p></a>
     </div>
     <div class="d-flex justify-content-center">{{$upcoming_data->links()}}</div>
     <div id="popular" class="row px-4">
         @foreach($upcoming_data as $item)
         @if(!empty($item->poster_path))
             <div  class="col-6 col-md-3 col-xl-2">
-            <a href="/play/{{$item->id}}"><img class="display_img" src="https://image.tmdb.org/t/p/w500{{$item->poster_path}}"  width="100%" height="100%" ></a>
+            <a href="play/{{$item->id}}"><img class="display_img" src="https://image.tmdb.org/t/p/w500{{$item->poster_path}}"  width="100%" height="100%" ></a>
             </div>
         @endif
         @endforeach
@@ -72,14 +72,14 @@
 @elseif(isset($dramas_data))
     <div class="movie_category pt-3 px-4">
         <p class="btn btn-danger">{{ $name }}</p>
-        <a href="/index"><p class="btn btn-dark btn-sm float-right ">戻る</p></a>
+        <a href="index"><p class="btn btn-dark btn-sm float-right ">戻る</p></a>
     </div>
     <div class="d-flex justify-content-center">{{$dramas_data->links()}}</div>
     <div id="popular" class="row px-4">
         @foreach($dramas_data as $item)
         @if(!empty($item->poster_path))
             <div  class="col-6 col-md-3 col-xl-2">
-            <a href="/play/{{$item->id}}"><img class="display_img" src="https://image.tmdb.org/t/p/w500{{$item->poster_path}}"  width="100%" height="100%" ></a>
+            <a href="play/{{$item->id}}"><img class="display_img" src="https://image.tmdb.org/t/p/w500{{$item->poster_path}}"  width="100%" height="100%" ></a>
             </div>
         @endif
         @endforeach
